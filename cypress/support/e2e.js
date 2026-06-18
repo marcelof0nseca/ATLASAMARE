f0c16eee2e6e95277e15cb01dadfbe39ea316951
marcelof0nseca <maf@cesar.school>
2026-06-18 17:17:38 -0300

    test: estabiliza abas e popup no cypress

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -7,4 +7,12 @@ Cypress.Commands.add("loginAs", (email) => {
   cy.get('input[name="email"]').type(email);
   cy.get('input[name="password"]').type("amare123!", { log: false });
   cy.get('button[type="submit"]').click();
+  cy.get("body[data-user-id]", { timeout: 20000 }).then(($body) => {
+    const userId = $body.attr("data-user-id");
+    if (userId) {
+      cy.window().then((win) => {
+        win.localStorage.setItem(`amare_notif_asked_${userId}`, "1");
+      });
+    }
+  });
 });
